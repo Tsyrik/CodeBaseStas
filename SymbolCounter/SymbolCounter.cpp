a770d70b8798d61313ecc4f149cadaae8b94974d
Tsyrik <forgamesandother@gmail.com>
2021-05-05 21:11:59 +0300

    Commiting logic

--- SymbolCounter/SymbolCounter.cpp
+++ SymbolCounter/SymbolCounter.cpp
@@ -1,6 +1,20 @@
 #include <iostream>
 
+int SymbolsCounter(const std::string& stringX, const char& charX) {
+    int res = 0;
+    for (int i = 0; i < stringX.size(); ++i) {
+        if (stringX[i] == charX) {
+            res = ++res;
+        }
+    };
+    return res;
+};
+
 int main()
 {
-    std::cout << "Hello World!\n";
+
+    std::string symbs = "Hellol";
+    char check = 'l';
+    int myRes = SymbolsCounter(symbs, check);
+    std::cout << myRes << std::endl;
 }
